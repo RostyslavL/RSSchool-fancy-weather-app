e220eca8fb2eef034e92e03d39bd0e327cb9fd3d
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-08-24 12:11:15 +0200

    fix: current location data correction

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,6 @@
 import CurrentLocation from'./CurrentLocation.js'
 import {addSpinner,displayError} from'./domFunctions.js'
+import {setLocationObject} from'./dataFunctions.js'
 
 const currentLoc = new CurrentLocation()
 
@@ -7,9 +8,14 @@ const initApp = () =>{
     // add listeners
     const geoButton = document.getElementById('getLocation')
     geoButton.addEventListener('click', getGeoWeather)
+
+    const homeButton = document.getElementById('home')
+    // homeButton.addEventListener('click', loadWeather)
     // set up
 
     //load weather
+    // loadWeather()
+    
 }
 
 document.addEventListener('DOMContentLoaded',initApp)
@@ -20,27 +26,28 @@ const getGeoWeather = (event) =>{
         const mapIcon = document.querySelector('.fa-map-marker-alt')
         addSpinner(mapIcon)
     }
-    if(!navigator.geolocation) {
-        geoError()
-    }else{
-        navigator.geolocation.getCurrentPosition(geoSuccess, geoError)
-    }
-
-    const geoError = (errObj) =>{
-        const errMsg = errObj ? errObj.message : 'Geolocation error.Geolocation is not supported'
-        displayError(errMsg, errMsg)
-    }
-
-    const geoSuccess = (position) =>{
-        const myCoordsObj = {
-            lat: position.coords.latitude,
-            lon: position.coords.longitude,
-            name:`Lat:${position.coords.latitude} Long:${position.coords.longitude}`
-        }
-        // set location object
-        setLocationObject(currentLoc, myCoordsObj)
-        // update data and display
-    }
+    if (!navigator.geolocation) return geoError()
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError)
 }
 
+const geoError = (errObj) =>{
+    const errMsg = errObj ? errObj.message : 'Geolocation error.Geolocation is not supported'
+    displayError(errMsg, errMsg)
+}
+
+const geoSuccess = (position) => {
+    const myCoordsObj = {
+      lat: position.coords.latitude,
+      lon: position.coords.longitude,
+      name: `Lat:${position.coords.latitude} Long:${position.coords.longitude}`
+    };
+    setLocationObject(currentLoc, myCoordsObj)
+    updateDataAndDisplay(currentLoc)
+    console.log(currentLoc)
+}
+      
+const updateDataAndDisplay = async (locationObj) =>{
+    // const weatherJson = await getWeatherFromCoords(locationobj)
+    // if(weatherJson) updateDisplay(weatherJson, locationObj)
+}
 
