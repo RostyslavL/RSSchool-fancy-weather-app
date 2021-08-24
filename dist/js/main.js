08496854a43d55e8c18eeca3744098aabfffe784
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-08-24 11:49:59 +0200

    feat: add geoError & geoSuccess with displayError messages functionality

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,5 @@
 import CurrentLocation from'./CurrentLocation.js'
-import {addSpinner} from'./domFunctions.js'
+import {addSpinner,displayError} from'./domFunctions.js'
 
 const currentLoc = new CurrentLocation()
 
@@ -20,11 +20,27 @@ const getGeoWeather = (event) =>{
         const mapIcon = document.querySelector('.fa-map-marker-alt')
         addSpinner(mapIcon)
     }
-    // if(!navigator.geolocation) {
-    //     geoError()
-    // }else{
-    //     navigator.geolocation.getCurrentPosition(geoSuccess, geoError)
-    // }
+    if(!navigator.geolocation) {
+        geoError()
+    }else{
+        navigator.geolocation.getCurrentPosition(geoSuccess, geoError)
+    }
+
+    const geoError = (errObj) =>{
+        const errMsg = errObj ? errObj.message : 'Geolocation error.Geolocation is not supported'
+        displayError(errMsg, errMsg)
+    }
+
+    const geoSuccess = (position) =>{
+        const myCoordsObj = {
+            lat: position.coords.latitude,
+            lon: position.coords.longitude,
+            name:`Lat:${position.coords.latitude} Long:${position.coords.longitude}`
+        }
+        // set location object
+        setLocationObject(currentLoc, myCoordsObj)
+        // update data and display
+    }
 }
 
 
