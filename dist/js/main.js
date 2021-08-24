c792dbd98af494aff7cdbebee18fc70da8643ee0
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-08-24 11:02:33 +0200

    feat: create CurrentLocation class , import this class into main js file

---
+++ dist/js/main.js
@@ -0,0 +1,16 @@
+import CurrentLocation from'./CurrentLocation.js';
+
+const currentLoc = new CurrentLocation();
+
+const initApp = () =>{
+    // add listeners
+
+    // set up
+
+    //load weather
+}
+
+document.addEventListener('DOMContentLoaded',initApp);
+
+
+
